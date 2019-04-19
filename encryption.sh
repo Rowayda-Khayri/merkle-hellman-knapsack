d579dc7465af4aa63e8c1bb42bdf915b1b33cf7a
Rowayda.Khayri <rowayda.khayri@gmail.com>
2019-04-19 21:41:55 +0200

    encryption algorithm

---
+++ encryption.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#take simple knapsack input
+
+#ensure that simple knapsack is superincreasing
+
+#calculate S sum
+
+#take w : w in [1, S sum]
+
+#ensure that w in [1, S sum]
+
+#take n : n > S sum and n is prime
+
+#ensure that n > S sum and n is prime
+
+#take the binary plain msg
+
+#ensure that msg is binary
+
+#generate hard knapsack (public key)
+
+#### Encryption ####
+
+
